45e9e8b3bade91ee59adbd95a51646725fedd56d
kiran-kharsel <kirankharsel@gmail.com>
2026-01-26 06:46:09 +0530

    chore: add quizata JS file for quiz arrays

---
+++ quizData.js
@@ -0,0 +1,38 @@
+// html quiz
+const htmlQuiz = [
+  {
+    id: 1,
+    question: "What does HTML stand for?",
+    answers: [
+      "Hyper Text Markup Language",
+      "High Tech Modern Language",
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language"
+    ],
+    correctAnswer: "Hyper Text Markup Language"
+  },
+  {
+    id: 2,
+    question: "Which HTML tag is used to define the largest heading?",
+    answers: ["<h6>", "<head>", "<h1>", "<heading>"],
+    correctAnswer: "<h1>"
+  },
+  {
+    id: 3,
+    question: "Which attribute is used to provide an alternative text for an image?",
+    answers: ["alt", "title", "src", "longdesc"],
+    correctAnswer: "alt"
+  },
+  {
+    id: 4,
+    question: "Which HTML element is used to create a hyperlink?",
+    answers: ["<link>", "<a>", "<href>", "<hyper>"],
+    correctAnswer: "<a>"
+  },
+  {
+    id: 5,
+    question: "Which tag is used to insert a line break in HTML?",
+    answers: ["<break>", "<lb>", "<br>", "<hr>"],
+    correctAnswer: "<br>"
+  }
+];
